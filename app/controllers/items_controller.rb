70e34791ef7871fb24787b4239e734cc101f3ce6
copanda-2007 <namaiki.2007@gmail.com>
2022-02-22 12:30:23 +0900

    rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    #binding.pry
+    # binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -20,8 +20,9 @@ class ItemsController < ApplicationController
   end
 
   private
-  def item_params
-    params.require(:item).permit(:description, :product, :price, :image, :category_id, :state_id, :delivery_charge_burden_id, :delivery_area_id, :delivery_day_id).merge(user_id: current_user.id)
-  end
 
+  def item_params
+    params.require(:item).permit(:description, :product, :price, :image, :category_id, :state_id, :delivery_charge_burden_id,
+                                 :delivery_area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,20 @@
 class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category              
-  belongs_to :state                 
+  belongs_to :category
+  belongs_to :state
   belongs_to :delivery_charge_burden
-  belongs_to :delivery_area         
-  belongs_to :delivery_day          
+  belongs_to :delivery_area
+  belongs_to :delivery_day
 
   validates :description,               presence: true
   validates :product,                   presence: true
-  validates :price,                     presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, presence: { message: "can't be blank"}
-  validates :category_id,               presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :state_id,                  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_charge_burden_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_area_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_day_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,                     presence: true, format: { with: /\A[0-9]+\z/ },
+                                        numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: { message: "can't be blank" }
+  validates :category_id,               presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id,                  presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_burden_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_area_id,          presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_day_id,           presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :image,                     presence: true
-  
 end
