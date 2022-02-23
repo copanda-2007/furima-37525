bf98d138eb1f5af3bad323597bb30ba7e0e35d03
copanda-2007 <namaiki.2007@gmail.com>
2022-02-23 21:55:51 +0900

    updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       @item.save
       redirect_to item_path(@item.id)
