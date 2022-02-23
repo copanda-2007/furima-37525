1c308b00ce3c61d7c26fa86ef7e6d185b560b563
copanda-2007 <namaiki.2007@gmail.com>
2022-02-23 16:12:58 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -18,10 +19,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show     
+  end
+  
+
   private
 
   def item_params
     params.require(:item).permit(:description, :product, :price, :image, :category_id, :state_id, :delivery_charge_burden_id,
                                  :delivery_area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+      @item = Item.find(params[:id])
   end
 end
