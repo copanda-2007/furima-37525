a64fa6426ceb68bf745afdfdacb3f773d4ae38b4
copanda-2007 <namaiki.2007@gmail.com>
2022-02-25 20:43:04 +0900

    Formオブジェクトパターン実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,27 @@
+class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase_delivery = PurchaseDelivery.new
+  end
+
+  def create
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_delivery).permit(:postcode, :delivery_area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
